b1db5bc0cf346802c78e5335543dfbdc3af1e571
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2025-03-11 09:56:42 -0500

    opengraph image editing again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         type='text/css'
         href='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css'
       />
+      <title>I'm André! Nice to finally meet!</title>
       <meta
         name='description'
         content='I solve problems with modern technology'
@@ -21,16 +22,23 @@ export default function Document() {
         property='og:description'
         content='I solve problems with modern technology'
       />
-      <meta property='og:image' content='./public/opengraph-image.png' />
+      <meta
+        property='og:image'
+        content='https://opengraph.b-cdn.net/production/images/c94aa915-2c3c-45f5-abc3-e8d4a228ef33.png?token=9ftoQWzEKO81HMi62couFKSfEmjp-4ty4qoaVsDcv5s&height=546&width=1200&expires=33277704558'
+      />
 
       <meta name='twitter:card' content='summary_large_image' />
       <meta property='twitter:domain' content='aguynamedandre.com' />
+      <meta property='twitter:url' content='https://aguynamedandre.com' />
       <meta name='twitter:title' content="I'm André! Nice to finally meet!" />
       <meta
         name='twitter:description'
         content='I solve problems with modern technology'
       />
-      <meta name='twitter:image' content='./public/opengraph-image.png' />
+      <meta
+        name='twitter:image'
+        content='https://opengraph.b-cdn.net/production/images/c94aa915-2c3c-45f5-abc3-e8d4a228ef33.png?token=9ftoQWzEKO81HMi62couFKSfEmjp-4ty4qoaVsDcv5s&height=546&width=1200&expires=33277704558'
+      />
 
       <body>
         <Main />
